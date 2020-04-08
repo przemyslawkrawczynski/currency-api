ead0073d82f0bea19816eeee453f71c12b907101
unknown <pk@bluzo.pl>
2020-04-08 12:22:58 +0200

    Change test suite class

--- src/test/java/CurrencyProviderTestSuits.java
+++ src/test/java/CurrencyProviderTestSuits.java
@@ -1,18 +1,18 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-import pl.ss.currency.api.CurrencyFromNbpXMLProviderTestSuits;
-import pl.ss.currency.dataprovider.XmlFromNbpApiDataProviderTestSuits;
-import pl.ss.currency.mapper.XmlResponseFromNbpMapperTestSuits;
-import pl.ss.currency.validator.ValidatorTestSuits;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-	CurrencyFromNbpXMLProviderTestSuits.class,
-	ValidatorTestSuits.class,
-	XmlResponseFromNbpMapperTestSuits.class,
-	XmlFromNbpApiDataProviderTestSuits.class,
-	CurrencyFromNbpXMLProviderTestSuits.class,
-})
-public class CurrencyProviderTestSuits {
-}
+//import org.junit.runner.RunWith;
+//		import org.junit.runners.Suite;
+//
+//		import pl.ss.currency.api.CurrencyFromNbpXMLProviderTestSuits;
+//		import pl.ss.currency.dataprovider.XmlFromNbpApiDataProviderTestSuits;
+//		import pl.ss.currency.mapper.XmlResponseFromNbpMapperTestSuits;
+//		import pl.ss.currency.validator.ValidatorTestSuits;
+//
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({
+//		CurrencyFromNbpXMLProviderTestSuits.class,
+//		ValidatorTestSuits.class,
+//		XmlResponseFromNbpMapperTestSuits.class,
+//		XmlFromNbpApiDataProviderTestSuits.class,
+//		CurrencyFromNbpXMLProviderTestSuits.class,
+//})
+//public class CurrencyProviderTestSuits {
+//}
